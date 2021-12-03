5436544509cf8239195aae5807e3dd8b956aa8ad
lingulist <mattis.list@lingulist.de>
2021-12-03 12:34:04 +0100

    adding tests

---
+++ test.py
@@ -0,0 +1,15 @@
+def test_valid(cldf_dataset, cldf_logger):
+    assert cldf_dataset.validate(log=cldf_logger)
+
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 21483 
+    assert any(f["Form"] == "ʔnui¹" for f in cldf_dataset["FormTable"])
+
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 207
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 94
